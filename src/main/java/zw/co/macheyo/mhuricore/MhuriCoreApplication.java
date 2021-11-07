3dbb15aee51c9dd16b61151a21073d3f3fc079f9
Kudzai Justice Macheyo <kudzai.macheyo@cassavasmartech.co.zw>
2021-11-07 13:30:52 +0200

    initial commit

---
+++ src/main/java/zw/co/macheyo/mhuricore/MhuriCoreApplication.java
@@ -0,0 +1,13 @@
+package zw.co.macheyo.mhuricore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhuriCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhuriCoreApplication.class, args);
+	}
+
+}
