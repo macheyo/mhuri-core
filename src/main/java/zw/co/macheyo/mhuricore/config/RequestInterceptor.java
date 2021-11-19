71370dfc217794096444008af5cc3dff139f8ac6
Kudzai Justice Macheyo <kudzai.macheyo@cassavasmartech.co.zw>
2021-11-19 20:53:28 +0200

    added Request Interceptor

---
+++ src/main/java/zw/co/macheyo/mhuricore/config/RequestInterceptor.java
@@ -0,0 +1,39 @@
+package zw.co.macheyo.mhuricore.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@Slf4j
+public class RequestInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object object) throws Exception {
+        log.info("In preHandle we are Intercepting the Request");
+        log.info("____________________________________________");
+        String requestURI = request.getRequestURI();
+        String tenantID = request.getHeader("X-TenantID");
+        log.info("RequestURI::" + requestURI +" || Search for X-TenantID  :: " + tenantID);
+        log.info("____________________________________________");
+        if (tenantID == null) {
+            response.getWriter().write("X-TenantID not present in the Request Header");
+            response.setStatus(400);
+            return false;
+        }
+        TenantContext.setCurrentTenant(tenantID);
+        return true;
+    }
+
+    @Override
+    public void postHandle(
+            HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
+            throws Exception {
+        TenantContext.clear();
+    }
+
+}
