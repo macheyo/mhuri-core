92d432b2aca05da876ca90c60c9f142db000f6fc
Kudzai Justice Macheyo <kudzai.macheyo@cassavasmartech.co.zw>
2021-11-19 21:22:37 +0200

    added hibernate configurations

---
+++ src/main/java/zw/co/macheyo/mhuricore/multiTenancy/config/TenantSchemaResolver.java
@@ -0,0 +1,26 @@
+package zw.co.macheyo.mhuricore.multiTenancy.config;
+
+import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+import org.springframework.stereotype.Component;
+import zw.co.macheyo.mhuricore.multiTenancy.TenantContext;
+
+@Component
+public class TenantSchemaResolver implements CurrentTenantIdentifierResolver {
+
+    private String defaultTenant ="public";
+
+    @Override
+    public String resolveCurrentTenantIdentifier() {
+        String t =  TenantContext.getCurrentTenant();
+        if(t!=null){
+            return t;
+        } else {
+            return defaultTenant;
+        }
+    }
+
+    @Override
+    public boolean validateExistingCurrentSessions() {
+        return true;
+    }
+}
