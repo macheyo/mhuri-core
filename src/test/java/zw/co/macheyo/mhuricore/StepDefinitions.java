3759414d482b56f3072183c338313b32185c1d03
Kudzai Justice Macheyo <kudzai.macheyo@cassavasmartech.co.zw>
2021-11-20 05:32:33 +0200

    finished off multi-tenant database setup

--- src/test/java/zw/co/macheyo/mhuricore/StepDefinitions.java
+++ src/test/java/zw/co/macheyo/mhuricore/StepDefinitions.java
@@ -5,11 +5,24 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitions {
-    @Given("spanner is available")
-    public void spanner_is_available() {
+    @Given("inventory availability is true")
+    public void inventory_availability_is_true() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
+    @Given("people are available")
+    public void people_are_available(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
 
     @When("Chipo receives cash")
     public void chipo_receives_cash() {
@@ -17,10 +30,11 @@ public class StepDefinitions {
         throw new io.cucumber.java.PendingException();
     }
 
+
     @Then("increase cash, sales account and adjust inventory")
     public void increase_cash_sales_account_and_adjust_inventory() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-
+    
 }
--- src/test/resources/zw/co/macheyo/mhuricore/cashSales.feature
+++ src/test/resources/zw/co/macheyo/mhuricore/cashSales.feature
@@ -1,5 +1,13 @@
 Feature: Cash Sales
-  Scenario: Tendai wants to buy a spanner
-    Given spanner is available in inventory
-    When Chipo receives cash
-    Then increase cash, sales account and adjust inventory
+
+  Mhuri allows a cashier to record cash sales
+
+  Rule: Cash sale should only be possible if inventory is available
+
+    Scenario: Inventory available
+      Given inventory availability is true
+      And people are available
+        | name    | Chipo   | Tendai    |
+        | role    | cashier | customer  |
+      When Chipo receives cash
+      Then increase cash, sales account and adjust inventory
