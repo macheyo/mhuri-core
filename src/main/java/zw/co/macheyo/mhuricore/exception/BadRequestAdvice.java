5136ab22369e30c88e8fb3a12153ea985ea9c0a6
Kudzai Justice Macheyo <kudzai.macheyo@cassavasmartech.co.zw>
2022-02-09 21:33:16 +0200

    added advice for Bad request exception

---
+++ src/main/java/zw/co/macheyo/mhuricore/exception/BadRequestAdvice.java
@@ -0,0 +1,27 @@
+package zw.co.macheyo.mhuricore.exception;
+
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class BadRequestAdvice {
+    @ResponseBody
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<?> BadRequestExceptionHandler(BadRequestException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create()
+                        .withTitle("Bad Request")
+                        .withDetail(ex.getMessage()));
+    }
+}
